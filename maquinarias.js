871690a48bf20fd1020a5279ab07a9e6b3bf8ec7
Cesar Sanchez <sanchezmi89@hotmail.com>
2022-05-12 17:17:35 -0500

    Se agrega el proyecto

---
+++ maquinarias.js
@@ -0,0 +1,46 @@
+var imagenes = ["imagenes/Banner-4.jpg", "imagenes/Banner-5.jfif", "imagenes/Banner-6.jpg" ]
+var cont = 0;
+
+function carrusel(contenedor){
+    contenedor.addEventListener("click", e =>{
+        var atras = contenedor.querySelector(".atras");
+        var adelante = contenedor.querySelector(".adelante");
+        var img = contenedor.querySelector("img");
+        var tgt = e.target;
+
+        if (tgt == atras){
+            if(cont > 0){
+                img.src = imagenes[cont-1];
+                cont--;
+            }else{
+                img.src = imagenes[imagenes.length - 1];
+                cont = imagenes.length - 1;
+            }
+        }else if (tgt == adelante){
+            if(cont < imagenes.length-1){
+                img.src = imagenes[cont+1];
+                cont++;
+            }else{
+                img.src = imagenes[0];
+                cont = 0;
+            }
+        }
+    })
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    var contenedor = document.querySelector(".contenedor");
+    carrusel(contenedor);
+});
+
+$(document).ready(function(){
+    var altura = $(".caja").offset().top;
+
+    $(window).on("scroll", function(){
+        if ($(window).scrollTop() > altura){
+            $(".caja").addClass("menu-fixed");
+        }else{
+            $(".caja").removeClass("menu-fixed");
+        }
+    });
+});
